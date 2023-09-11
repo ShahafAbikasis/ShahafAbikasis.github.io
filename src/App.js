9482085e3752e03e8bdda08e379550244bb740b9
Yael Tzruia <yaeli.rogestav@gmail.com>
2023-09-11 11:02:36 +0300

    first push

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import { ChakraProvider, Box } from '@chakra-ui/react';
+import './themes/style.css';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { Route, Routes } from 'react-router-dom';
+import routes from './config/routes';
+import theme from './themes/themes';
+import PageProvider from './context/pageContext';
+import usePage from './hooks/usePage';
+import { AnimatePresence } from 'framer-motion';
+// import { RtlProvider } from './context/RTLContext';
 
 function App() {
+  const page = usePage();
+
   return (
     <ChakraProvider theme={theme}>
+      {/* <RtlProvider> */}
       <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
+        <PageProvider.Provider value={page}>
+          <AnimatePresence mode="wait">
+            <Routes>
+              {routes.map(route => (
+                <Route
+                  path={route.path}
+                  key={route.path}
+                  element={route.component}
+                />
+              ))}
+              {/* <Route path="*" element={<Navigate to="/feed" />} /> */}
+            </Routes>
+          </AnimatePresence>
+        </PageProvider.Provider>
       </Box>
+      {/* </RtlProvider> */}
     </ChakraProvider>
   );
 }
